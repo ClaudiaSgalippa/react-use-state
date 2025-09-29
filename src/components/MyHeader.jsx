a58f8d6c81ff135b8736dbb0ec44771aa6580626
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-09-29 17:59:16 +0200

    Prima impostazione

---
+++ src/components/MyHeader.jsx
@@ -0,0 +1,14 @@
+const MyHeader = () => {
+    
+    return (
+        <>
+            <header>
+                <div className="onlyHeader">
+                    <h1>Learn Web development</h1>
+                </div>
+            </header>
+        </>
+    )
+};
+
+export default MyHeader
---
+++ src/components/MyMain.jsx
@@ -0,0 +1,14 @@
+const MyMain = () => {
+    
+    return (
+        <>
+            <main>
+                <div className="mainContainer">
+                    <h1>Qui andrà la card</h1>
+                </div>
+            </main>
+        </>
+    )
+};
+
+export default MyMain
